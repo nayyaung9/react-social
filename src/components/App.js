8b0542945dc9a20a58e208c8239c4620f923e8f0
nayyaung9 <naygit.developer@gmail.com>
2019-10-08 00:21:26 +0630

    firebase authentication done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,40 @@
 import React from 'react'
 
-class App extends React.Components {
+// Auth
+import withFirebaseAuth from 'react-with-firebase-auth';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from '../firebase';
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const firebaseAppAuth = firebaseApp.auth();
+
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
+
+class App extends React.Component {
   render() {
+    const { user, signOut, signInWithGoogle } = this.props;
     return (
-      <div>App</div>
+      <div>
+        {
+          user 
+          ? <div>hello, {user.displayName}</div>
+          : <p>Please sign in</p>
+        }
+        {
+          user
+          ? <button onClick={signOut}>Sign Out</button>
+          : <button onClick={signInWithGoogle}>Sign in with Google</button>
+        }
+      </div>
     )
   }
 }
 
-export default App;
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App);
