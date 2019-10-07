b715af862cb02c363ca8d7372a7ece26464b0865
nayyaung9 <naygit.developer@gmail.com>
2019-10-08 00:09:56 +0630

    config firebase

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class App extends React.Components {
+  render() {
+    return (
+      <div>App</div>
+    )
+  }
+}
+
+export default App;
---
+++ src/firebase/index.js
@@ -0,0 +1,8 @@
+const config = {
+  apiKey: 'AIzaSyCR8t5_A0nMSQmDUNamIuF92A9jObUqCjw',
+  authDomain: 'react-social-ab70b.firebaseapp.com',
+  databaseURL: 'https://react-social-ab70b.firebaseio.com/',
+  projectId: 'react-social-ab70b'
+}
+
+export default config;
