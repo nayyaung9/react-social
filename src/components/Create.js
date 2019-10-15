b7ece6aad2794b6f647dd8414a7e40debaa72813
nayyaung9 <naygit.developer@gmail.com>
2019-10-16 00:07:43 +0630

    run build script for firebase deploy

--- src/components/Create.js
+++ src/components/Create.js
@@ -2,36 +2,28 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import firebase from 'firebase';
+
 import * as actions from '../actions/';
 import { connect } from 'react-redux';
+
+import firebase from 'firebase';
 import FileUploader from "react-firebase-file-uploader";
 
 class Create extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: '',
-      content: '',
-      image: null
-    }
-  }
 
-  // handleChange = e => {
-  //   if (e.target.files[0]) {
-  //     const image = e.target.files[0];
-  //     this.setState({image});
-  //     console.log(this.state.image)
-  //   }
-  // };
+  state = {
+    title: '',
+    content: '',
+    image: null
+  }
 
   handleImage = filename => {
     firebase
-    .storage()
-    .ref("images")
-    .child(filename)
-    .getDownloadURL()
-    .then(url => this.setState({ image: url }));
+      .storage()
+      .ref("images")
+      .child(filename)
+      .getDownloadURL()
+      .then(url => this.setState({ image: url }));
   }
 
   onHandleSubmit = event => {
@@ -43,7 +35,6 @@ class Create extends React.Component {
     this.setState({
       title: '',
       content: '',
-      photo: '',
     })
   }
 
@@ -61,7 +52,6 @@ class Create extends React.Component {
               margin="normal"
               variant="outlined"
               fullWidth={true}
-              value={this.state.title}
               defaultValue={this.state.title}
               onChange={event => {
                 const { value } = event.target;
@@ -77,7 +67,6 @@ class Create extends React.Component {
               margin="normal"
               variant="outlined"
               fullWidth={true}
-              value={this.state.content}
               defaultValue={this.state.content}
               onChange={event => {
                 const { value } = event.target;
@@ -85,9 +74,10 @@ class Create extends React.Component {
               }}
               required
             />
-              {/* <input type="file" onChange={this.handleImage} />
-             */}
-             <FileUploader storageRef={firebase.storage().ref("images")} onUploadSuccess={this.handleImage} />
+              <FileUploader 
+                storageRef={firebase.storage().ref("images")} 
+                onUploadSuccess={this.handleImage} 
+              />
             <Button type="submit" variant="outlined" color="primary">Submit</Button>
           </form>
         </Container>
@@ -97,66 +87,3 @@ class Create extends React.Component {
 }
 
 export default connect(null, actions)(Create);
-
-// import React, { Component } from "react";
-// import firebase from "firebase";
-// import FileUploader from "react-firebase-file-uploader";
- 
-// class ProfilePage extends Component {
-//   state = {
-//     username: "",
-//     avatar: "",
-//     isUploading: false,
-//     progress: 0,
-//     avatarURL: ""
-//   };
- 
-//   handleChangeUsername = event =>
-//     this.setState({ username: event.target.value });
-//   handleUploadStart = () => this.setState({ isUploading: true, progress: 0 });
-//   handleProgress = progress => this.setState({ progress });
-//   handleUploadError = error => {
-//     this.setState({ isUploading: false });
-//     console.error(error);
-//   };
-//   handleUploadSuccess = filename => {
-//     this.setState({ avatar: filename, progress: 100, isUploading: false });
-//     firebase
-//       .storage()
-//       .ref("images")
-//       .child(filename)
-//       .getDownloadURL()
-//       .then(url => this.setState({ avatarURL: url }));
-//   };
- 
-//   render() {
-//     return (
-//       <div style={{ marginTop: 120 }}>
-//         <form>
-//           <label>Username:</label>
-//           <input
-//             type="text"
-//             value={this.state.username}
-//             name="username"
-//             onChange={this.handleChangeUsername}
-//           />
-//           <label>Avatar:</label>
-//           {this.state.isUploading && <p>Progress: {this.state.progress}</p>}
-//           {this.state.avatarURL && <img src={this.state.avatarURL} />}
-//           <FileUploader
-//             accept="image/*"
-//             name="avatar"
-//             randomizeFilename
-//             storageRef={firebase.storage().ref("images")}
-//             onUploadStart={this.handleUploadStart}
-//             onUploadError={this.handleUploadError}
-//             onUploadSuccess={this.handleUploadSuccess}
-//             onProgress={this.handleProgress}
-//           />
-//         </form>
-//       </div>
-//     );
-//   }
-// }
- 
-// export default ProfilePage;
